26ae819d1939935e12c6cb3486c6d15512f2685d
jalil <reachjalil@gmail.com>
2017-11-10 14:02:54 -0700

    added autocomplete for parenthesis as well as some more related functionality

--- src/app/classes/mode.ts
+++ src/app/classes/mode.ts
@@ -6,11 +6,13 @@ export class Mode {
 		var MatchingBraceOutdent = require("ace/mode/matching_brace_outdent").MatchingBraceOutdent;
     	var betterformulaCompletions = require("ace/mode/betterformula_completions").BetterformulaCompletions;
 		var FoldMode = require("ace/mode/folding/better_formula_fold_mode").FoldMode;
+		var CstyleBehaviour =require("ace/mode/behaviour/cstyle").CstyleBehaviour;
 		var betterFormulaMode = function(){
     		    this.HighlightRules = HighlightRules;
     		    this.$outdent = new MatchingBraceOutdent();
     		    this.foldingRules = new FoldMode();
     		    this.$completer = new betterformulaCompletions();
+    			this.$behaviour = new CstyleBehaviour();
 		}
 		oop.inherits(betterFormulaMode, TextMode);
 		(function() {
